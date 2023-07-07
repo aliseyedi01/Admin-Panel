7ee5ab7b7d8af59f5ba1b42854ba8b8fc320a791
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-07-07 18:57:38 +0330

    feat(antd) : add ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "antd";
 
 function App() {
   return (
     <>
       <h2 className="  text-center text-red-500">admin panel</h2>
+      <Button>secondary</Button>
     </>
   );
 }
