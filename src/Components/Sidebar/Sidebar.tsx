ed661ff5434820b5893df4f6780e7cb8cbe4c035
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-07-15 09:58:35 +0330

    fix ( sidebar ) : solve to show name in mobile and add background blur

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 // react
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 // antd
 import { Button } from "antd";
@@ -13,10 +13,10 @@ import {
   FaChevronRight,
   FaDesktop,
 } from "react-icons/fa";
+// redux
 import { useDispatch, useSelector } from "react-redux";
-// ref
-
 import { close } from "@/store/slice/menuSlice";
+// hook
 import useHideClickOutside from "@/Hooks/useHideClickOutside";
 
 interface Page {
@@ -37,7 +37,6 @@ const Sidebar: React.FC = () => {
 
   // redux
   const menuOpen = useSelector((state) => state.menu.isOpenMenu);
-  // console.log("menu", menuOpen);
 
   // hidden menu in outSide clicked
   const sidebarRef = useRef<HTMLDivElement>(null);
@@ -48,40 +47,51 @@ const Sidebar: React.FC = () => {
   useHideClickOutside(sidebarRef, handleHideSidebar);
 
   return (
-    <div
-      ref={sidebarRef}
-      className={`surface flex h-full flex-col items-center   gap-4 py-4 transition-all duration-500   ${
-        collapsed ? "w-20" : "w-96"
-      } ${menuOpen ? "absolute z-50 w-56" : "hidden md:flex"}`}
-    >
-      <Button
-        onClick={() => {
-          setCollapsed(!collapsed);
-        }}
-        className="absolute translate-x-4  self-end rounded-md border-2 border-sky-200 bg-transparent pb-7 text-lg dark:text-yellow-300 max-md:hidden"
-        icon={!collapsed ? <FaChevronLeft /> : <FaChevronRight />}
-      />
-      <h2 className=" flex items-center gap-3 self-center font-sans text-xl dark:text-white">
-        <FaDesktop className="text-2xl" /> {!collapsed ? " Admin Panel" : null}
-      </h2>
+    <>
+      {/* background blur */}
+      <div
+        className={`fixed left-0 top-0 z-10  h-full w-full bg-slate-600/[.2] backdrop-blur-sm  ${
+          menuOpen ? "" : "hidden"
+        }`}
+        onClick={() => dispatch(close())}
+      ></div>
+      {/* sidebar */}
+      <div
+        ref={sidebarRef}
+        className={`surface flex h-full flex-col items-center   gap-4 py-4 transition-all duration-500   ${
+          collapsed ? "w-20" : "w-96"
+        } ${menuOpen ? "absolute z-50 w-48" : "hidden md:flex"}`}
+      >
+        <Button
+          onClick={() => {
+            setCollapsed(!collapsed);
+          }}
+          className="absolute translate-x-4  self-end rounded-md border-2 border-sky-200 bg-transparent pb-7 text-lg dark:text-yellow-300 max-md:hidden"
+          icon={!collapsed ? <FaChevronLeft /> : <FaChevronRight />}
+        />
+        <h2 className=" flex items-center gap-3 self-center font-sans text-base dark:text-white md:text-xl">
+          <FaDesktop className="text-2xl" /> {!collapsed ? " Admin Panel" : null}
+        </h2>
 
-      <div className="mt-2 w-full px-2">
-        {Pages.map((page) => (
-          <Link
-            key={page.id}
-            to={page.name.toLowerCase()}
-            className="flex w-full items-center justify-center gap-3 no-underline"
-          >
-            <Button
-              type="primary"
-              className="my-[1px]  flex h-8 w-full items-center gap-3 bg-blue-500 "
+        <div className="mt-2 w-full px-2">
+          {Pages.map((page) => (
+            <Link
+              key={page.id}
+              to={page.name.toLowerCase()}
+              className="flex w-full items-center justify-center gap-3 no-underline"
             >
-              <div className="translate-x-1">{page.icon}</div> {!collapsed ? page.name : null}
-            </Button>
-          </Link>
-        ))}
+              <Button
+                type="primary"
+                className="my-[1px]  flex h-8 w-full items-center gap-3 bg-blue-500 "
+              >
+                <div className="translate-x-1">{page.icon}</div>{" "}
+                {!collapsed || menuOpen ? page.name : null}
+              </Button>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
