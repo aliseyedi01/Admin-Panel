e7e8ee42ded4b4fc6ab37c554c842370881fb5e8
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-07-31 19:41:08 +0330

    feat ( highcharts) : add highcharts for handle charts

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,72 @@
 import { PageLayout } from "@/Components";
 import Stats from "./Stats";
 
+// import Highcharts from "highcharts";
+import Highcharts from "highcharts/highstock";
+import HighchartsReact from "highcharts-react-official";
+import { useEffect, useState, useMemo } from "react";
+
+import HCExporting from "highcharts/modules/exporting";
+
+// Initialize the exporting module
+HCExporting(Highcharts);
+
 const Dashboard: React.FC = () => {
+  const [chartData, setChartData] = useState<number[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://demo-live-data.highcharts.com/aapl-c.json");
+      const data = await response.json();
+      setChartData(data);
+      // console.log(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const memoizedChartData = useMemo(() => chartData, [chartData]);
+
+  const options = {
+    chart: {
+      styledMode: true,
+    },
+    title: {
+      text: "Sale Quantity",
+    },
+    xAxis: {},
+    yAxis: {
+      title: {
+        text: "Quantity",
+      },
+      opposite: false,
+    },
+    series: [
+      {
+        name: "Sale",
+        data: memoizedChartData,
+      },
+    ],
+    exporting: {
+      enabled: true,
+    },
+  };
+
   return (
     <PageLayout>
       <Stats />
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+        constructorType={"stockChart"}
+        containerProps={{
+          className: "rounded-md overflow-hidden my-2  highcharts-dark",
+        }}
+      />
     </PageLayout>
   );
 };
