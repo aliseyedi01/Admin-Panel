2f6aa7bd0ec665c8e529946e9aca8b4d61a0e772
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-08-10 16:58:38 +0330

    feat ( supabase ) : remove edit functionality of blogs

--- src/Pages/Blog/BlogSingle.tsx
+++ src/Pages/Blog/BlogSingle.tsx
@@ -1,5 +1,5 @@
 // react
-import React, { useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import { PageLayout, BackPage, BlogForm } from "@/Components";
 // types
@@ -7,31 +7,19 @@ import { useAppDispatch, useAppSelector } from "@/interface/utils";
 import { editBlog } from "@/store/slice/blogSlice";
 // antd
 import { message } from "antd";
-// api
-import { useEditBlogQuery } from "@/store/api/blogsApi";
-import { BlogType } from "@/interface/blog";
 
 const BlogSingle: React.FC = () => {
   const dispatch = useAppDispatch();
   const param = useParams();
   const blogs = useAppSelector((state) => state.blog);
   const editProduct = blogs.find((blog) => blog.key === param.blogId)!;
-  // state for supabase
-  const [updatedBlog, setUpdatedBlog] = useState<{ blog: BlogType } | undefined>();
-  const [keyBlog, setKeyBlog] = useState<string>();
-
-  // Move the hook call here
-  const { data } = useEditBlogQuery({ updateBlog: updatedBlog, key: keyBlog });
 
   const onFinish = (value: any) => {
-    // set for api supabase
-    setUpdatedBlog(value.blog);
-    setKeyBlog(param.blogId);
     // set key for update slice redux
     const updatedProduct = { key: param.blogId, ...value.blog };
     dispatch(editBlog(updatedProduct));
     // message
-    message.success(`${value.blog.name} : edited successfully`);
+    message.success(`${value.blog.name} : Edited Successfully`);
   };
 
   return (
