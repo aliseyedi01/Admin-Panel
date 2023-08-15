48be49140eeba9bea0ab56fc46f84e0a15712a11
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-08-15 23:35:47 +0330

    fix ( main ) : use HashRouter for solve 404 Error

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./main.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
