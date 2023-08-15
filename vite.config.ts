c080366f893c5c23b1d9a9734bef93103fbb8484
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-08-15 21:09:21 +0330

    setup gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Admin-Panel/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
